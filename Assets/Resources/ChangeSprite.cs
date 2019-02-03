029d627d26be0f87e2846aa166c3f9b3859674ec
yj-wong <yujuewong15@gmail.com>
2019-02-03 08:09:52 -0500

    init

---
+++ Assets/Resources/ChangeSprite.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeSprite : MonoBehaviour
+{
+    public Sprite sprite2;
+    public void change()
+    {
+        Debug.Log(gameObject);
+    }
+}
